902eadca41d7b05ed716e32c22568c3306c1ba26
wenlianghuang <wenliangmatt@gmail.com>
2023-05-13 10:12:12 +0800

    test

--- pythonTest.py
+++ pythonTest.py
@@ -23,6 +23,7 @@ if __name__ == "__main__":
     logger.setLevel(logging.DEBUG)
 
     ch = logging.FileHandler("/app/logs/Test_Logs_{}_{}_{}_{}_{}_{}.log".format(time_date, time_date, time_date_time[0], time_date_time[1], time_date_time[2], time_date_millisecond))
+    # command
     #ch = logging.FileHandler("./test/logs/Test_Logs_{}_{}_{}_{}_{}_{}.log".format(time_date, time_date, time_date_time[0], time_date_time[1], time_date_time[2], time_date_millisecond))
     ch.setLevel(logging.DEBUG)
     formatter = logging.Formatter('[%(asctime)s  %(levelname)s  %(message)s]')
